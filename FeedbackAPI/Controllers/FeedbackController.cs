4d4da6e8ae3f539069369809b919d7e63fd45ba9
eslam3mad <aslamalrys9@gmail.com>
2026-04-13 22:37:06 +0200

    My Task

---
+++ FeedbackAPI/Controllers/FeedbackController.cs
@@ -0,0 +1,36 @@
+﻿using FeedbackAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<FeedbackController> _logger;
+
+        public FeedbackController(IConfiguration configuration, ILogger<FeedbackController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult SubmitFeedback([FromQuery] int Rate, [FromBody] FeedbackRequest request)
+        {
+            request.Rate = Rate;
+
+            var systemName = _configuration["SystemSettings:SystemName"];
+
+            _logger.LogInformation("Feedback received from user: {UserName}", request.UserName);
+
+            if (request.Rate < 3)
+            {
+                _logger.LogWarning("User is not satisfied");
+            }
+
+            return Ok($"Feedback received for {systemName}. Thank you, {request.UserName}!");
+        }
+    }
+}
---
+++ FeedbackAPI/DTOs/FeedbackRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FeedbackAPI.DTOs
+{
+    public class FeedbackRequest
+    {
+        public string UserName { get; set; }
+        public int Rate { get; set; }
+        public string Comment { get; set; }
+    }
+}
